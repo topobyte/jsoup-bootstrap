7cec33f32749fe94b09f07036d599a5abf598550
Sebastian Kürten <sebastian@topobyte.de>
2018-03-14 10:58:56 +0100

    Add utilities for adding CDN headers

---
+++ src/main/java/de/topobyte/jsoup/Bootstrap3.java
@@ -0,0 +1,23 @@
+
+package de.topobyte.jsoup;
+
+import de.topobyte.jsoup.nodes.Element;
+
+public class Bootstrap3
+{
+
+	public static void addCdnHeaders(Element head)
+	{
+		ElementUtil.appendFragmentHead(head,
+				"<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">");
+
+		ElementUtil.appendFragmentHead(head,
+				"<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>");
+		ElementUtil.appendFragmentHead(head,
+				"<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>");
+
+		ElementUtil.appendFragmentHead(head,
+				"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">");
+	}
+
+}
---
+++ src/main/java/de/topobyte/jsoup/Bootstrap4.java
@@ -0,0 +1,41 @@
+// Copyright 2018 Sebastian Kuerten
+//
+// This file is part of jsoup-bootstrap.
+//
+// jsoup-bootstrap is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// jsoup-bootstrap is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with jsoup-bootstrap. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.jsoup;
+
+import de.topobyte.jsoup.nodes.Element;
+
+public class Bootstrap4
+{
+
+	public static void addCdnHeaders(Element head)
+	{
+		ElementUtil.appendFragmentHead(head,
+				"<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">");
+
+		ElementUtil.appendFragmentHead(head,
+				"<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>");
+		ElementUtil.appendFragmentHead(head,
+				"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js\" integrity=\"sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q\" crossorigin=\"anonymous\"></script>");
+		ElementUtil.appendFragmentHead(head,
+				"<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js\" integrity=\"sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl\" crossorigin=\"anonymous\"></script>");
+
+		ElementUtil.appendFragmentHead(head,
+				"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">");
+	}
+
+}
