5ce85277c80c85d75a7bb15c3817f6e02f0d8539
Sebastian Kürten <sebastian@topobyte.de>
2020-01-24 16:38:40 +0100

    Upgrade jsoup-utils and use FormUtil.addPleaseSelectOption()

--- src/test/java/de/topobyte/jsoup/bootstrap3/FormsGenerator.java
+++ src/test/java/de/topobyte/jsoup/bootstrap3/FormsGenerator.java
@@ -26,13 +26,13 @@ import java.util.Arrays;
 
 import org.jsoup.nodes.Document;
 
+import de.topobyte.jsoup.FormUtil;
 import de.topobyte.jsoup.HTML;
 import de.topobyte.jsoup.bootstrap3.components.Container;
 import de.topobyte.jsoup.bootstrap3.forms.InputGroup;
 import de.topobyte.jsoup.bootstrap3.forms.SelectGroup;
 import de.topobyte.jsoup.components.Form;
 import de.topobyte.jsoup.components.Input.Type;
-import de.topobyte.jsoup.components.Option;
 import de.topobyte.jsoup.nodes.Element;
 
 public class FormsGenerator extends BaseGenerator
@@ -102,12 +102,7 @@ public class FormsGenerator extends BaseGenerator
 				"pet", "Preferred Pet", Arrays.asList("Cat", "Dog", "Bird"),
 				Arrays.asList("cat", "dog", "bird"));
 
-		Option initialOption = HTML.option();
-		selectGroup.getSelect().prependChild(initialOption);
-		initialOption.text("Please select");
-		initialOption.setSelected(true);
-		initialOption.attr("disabled", true);
-		initialOption.attr("hidden", true);
+		FormUtil.addPleaseSelectOption(selectGroup.getSelect());
 
 		BootstrapFormsHorizontal.addRadio(form, "dring", "Preferred Drink",
 				Arrays.asList("Coke", "Water", "Beer"),
